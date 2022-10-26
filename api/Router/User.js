e8ca4437b4fa226d251133d9a9ebb7be5e14e922
shivakantyusoft <shivakantyuvasoft357@gmail.com>
2022-10-26 18:38:34 +0530

    uncomment login code

--- api/Router/User.js
+++ api/Router/User.js
@@ -34,44 +34,44 @@ router.post("/signup",(req,res,next)=>{
 })
 
 
-// router.post("/login",(req,res,next)=>{
-//     User.find({username:req.body.username})
-//     .exec()
-//     .then((user)=>{
-//         if(user.length < 1){
-//             return res.status(401).json({
-//                 msg:"user does not exists"
-//             })
-//         }
-//         bcript.compare(req.body.password,user[0].password,(err,result)=>{
-//             if(!result){
-//                 res.status(401).json({
-//                     msg:"Password matching Failed"
-//                 })
-//             }
-//             if(result){
-//                 const token = jwt.sign({
-//                     username:user[0].username,
-//                     phone:user[0].phone
-//                 },
-//                 'crud token',
-//                 {
-//                     expiresIn:"24hours"
-//                 }
-//                 )
-//                 res.status(200).json({
-//                     username:user[0].username,
-//                     phone:user[0].phone,
-//                     token:token
-//                 })
-//             }
-//         })
-//     }).catch((err)=>{
-//         res.status(500).json({
-//             error:err
-//         })
-//     })
-// })
+router.post("/login",(req,res,next)=>{
+    User.find({username:req.body.username})
+    .exec()
+    .then((user)=>{
+        if(user.length < 1){
+            return res.status(401).json({
+                msg:"user does not exists"
+            })
+        }
+        bcript.compare(req.body.password,user[0].password,(err,result)=>{
+            if(!result){
+                res.status(401).json({
+                    msg:"Password matching Failed"
+                })
+            }
+            if(result){
+                const token = jwt.sign({
+                    username:user[0].username,
+                    phone:user[0].phone
+                },
+                'crud token',
+                {
+                    expiresIn:"24hours"
+                }
+                )
+                res.status(200).json({
+                    username:user[0].username,
+                    phone:user[0].phone,
+                    token:token
+                })
+            }
+        })
+    }).catch((err)=>{
+        res.status(500).json({
+            error:err
+        })
+    })
+})
 
 
 module.exports = router;
